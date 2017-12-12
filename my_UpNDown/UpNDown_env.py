028824f4c374879f05986be45d5469cdfa007da1
Hengjia Zhang <hengjia@umich.edu>
2017-12-12 00:45:39 -0500

    update UpNDown_env.py

--- my_UpNDown/UpNDown_env.py
+++ my_UpNDown/UpNDown_env.py
@@ -8,22 +8,26 @@ class my_UpNDownEnv(AtariEnv):
         self.observation_space = spaces.Box(low = 0, high = 255, shape = (840, 160, 3))
 
     def _step(self, action):
-        ob_list = np.array([])
+        ob_list = np.zeros((840, 160, 3))
         reward_sum = 0
         for i in range(4):
             observation, reward, done, info = \
                 super(my_UpNDownEnv, self)._step(action)
             #print(reward)
             #print(info)
-            if i == 0:
-                ob_list = observation
-            else:
-                ob_list = np.append(ob_list, observation, axis = 0)
+            # print(observation.shape)
+            # print(ob_list.shape)
+            ob_list[i * 210:(i+1)*210, :, :] = observation
+            # if i == 0:
+            #     ob_list = observation
+            # else:
+            #     ob_list = np.append(ob_list, observation, axis = 0)
             reward_sum += reward
             if done:
                 if i != 3:
                     for _ in range(i+1, 4):
-                        ob_list = np.append(ob_list, observation, axis = 0)
+                        ob_list[i * 210:(i+1)*210, :, :]= observation
+                        # ob_list = np.append(ob_list, observation, axis = 0)
                         reward_sum += reward
                 break
 
