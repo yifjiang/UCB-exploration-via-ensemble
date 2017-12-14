2679dbd387d3cd43ec4949e4a1d151a236cbfe21
gzhaojie <gzjmail1995@gmail.com>
2017-12-14 11:25:08 -0500

    plot

--- plot.py
+++ plot.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-file = open(sys.argv[1],'rb')
-returns = pickle.load(file)
-plt.plot(returns, label = "learning curve")
+file = open("",'rb')
+mean_frame100_rewards, mean_episode100_rewards, episode_rewards, frame_rewards = pickle.load(file)
+l = []
+l[0] = frame_rewards[0]
+for i in range(1, len(frame_rewards)):
+	l[i] = l[i-1] + frame_rewards[i]
+plt.plot(l, label = "learning curve")
 plt.legend()
 plt.show()
