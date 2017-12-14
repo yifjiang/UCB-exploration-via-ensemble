793898d6a92fba50e3232b8991625250b8f6cc86
yifjiang <yifjiang@umich.edu>
2017-12-13 22:40:55 -0500

    checkpoint

--- bootstrapped_DQN/models.py
+++ bootstrapped_DQN/models.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
-
+import sys
 
 def _mlp(hiddens, inpt, num_actions, scope, reuse=False, layer_norm=False):
     with tf.variable_scope(scope, reuse=reuse):
@@ -30,7 +30,7 @@ def mlp(hiddens=[], layer_norm=False):
     return lambda *args, **kwargs: _mlp(hiddens, layer_norm=layer_norm, *args, **kwargs)
 
 
-def _cnn_to_mlp(convs, hiddens, dueling, inpt, num_actions, scope, reuse=False, layer_norm=False):
+def _cnn_to_mlp(convs, hiddens, dueling, inpt, num_actions, scope, reuse=False, layer_norm=False, num_head=10, exploration_weight = 0.1):
     with tf.variable_scope(scope, reuse=reuse):
         out = inpt
         with tf.variable_scope("convnet"):
@@ -41,16 +41,24 @@ def _cnn_to_mlp(convs, hiddens, dueling, inpt, num_actions, scope, reuse=False, 
                                            stride=stride,
                                            activation_fn=tf.nn.relu)
         conv_out = layers.flatten(out)
-        with tf.variable_scope("action_value"):
-            action_out = conv_out
-            for hidden in hiddens:
-                action_out = layers.fully_connected(action_out, num_outputs=hidden, activation_fn=None)
-                if layer_norm:
-                    action_out = layers.layer_norm(action_out, center=True, scale=True)
-                action_out = tf.nn.relu(action_out)
-            action_scores = layers.fully_connected(action_out, num_outputs=num_actions, activation_fn=None)
+        action_scores_collection = []
+        for i in range(0,num_head):
+            with tf.variable_scope("action_value_" + str(i)):
+                action_out = conv_out
+                for hidden in hiddens:
+                    action_out = layers.fully_connected(action_out, num_outputs=hidden, activation_fn=None)
+                    if layer_norm:
+                        action_out = layers.layer_norm(action_out, center=True, scale=True)
+                    action_out = tf.nn.relu(action_out)
+                action_scores = layers.fully_connected(action_out, num_outputs=num_actions, activation_fn=None)
+                action_scores_collection.append(action_scores)
+        action_scores_stack = tf.stack(action_scores_collection)
+        action_scores_mean, action_scores_variance = tf.nn.moments(action_scores_stack, axes = [0])
+        action_scores_stdv = tf.sqrt(action_scores_variance)
 
         if dueling:
+            print("dueling:unhandled")
+            sys.exit()
             with tf.variable_scope("state_value"):
                 state_out = conv_out
                 for hidden in hiddens:
@@ -63,7 +71,7 @@ def _cnn_to_mlp(convs, hiddens, dueling, inpt, num_actions, scope, reuse=False, 
             action_scores_centered = action_scores - tf.expand_dims(action_scores_mean, 1)
             q_out = state_score + action_scores_centered
         else:
-            q_out = action_scores
+            q_out = action_scores + exploration_weight * action_scores_stdv
         return q_out
 
 
